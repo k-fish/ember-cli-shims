38c930bef21022a1276ef0fe28bffaf4a4235db4
k-fish <k-fish@users.noreply.github.com>
2016-09-02 14:43:09 -0400

    Include ember-data shims in later version

--- app-shims.js
+++ app-shims.js
@@ -241,6 +241,47 @@
       }
     }
   }
+  
+    function processEmberDataShims() {
+    var shims = {
+      'ember-data':                          '',
+      'ember-data/model':                    'Model',
+      'ember-data/serializers/rest':         'RESTSerializer',
+      'ember-data/serializers/active-model': 'ActiveModelSerializer',
+      'ember-data/serializers/json':         'JSONSerializer',
+      'ember-data/serializers/json-api':     'JSONAPISerializer',
+      'ember-data/adapters/json-api':        'JSONAPIAdapter',
+      'ember-data/adapters/rest':            'RESTAdapter',
+      'ember-data/adapter':                  'Adapter',
+      'ember-data/adapters/active-model':    'ActiveModelAdapter',
+      'ember-data/store':                    'Store',
+      'ember-data/transform':                'Transform',
+      'ember-data/attr':                     'attr',
+      'ember-data/relationships':            ['hasMany', 'belongsTo']
+    };
+
+    for (var moduleName in shims) {
+      generateLazyModule('DS', moduleName, shims[moduleName]);
+    }
+
+    if (Ember.Test) {
+      var testShims = {
+        'ember-test': {
+          'default': Ember.Test
+        },
+        'ember-test/adapter': {
+          'default': Ember.Test.Adapter
+        },
+        'ember-test/qunit-adapter': {
+          'default': Ember.Test.QUnitAdapter
+        }
+      };
+
+      for (var moduleName in testShims) {
+        generateModule(moduleName, testShims[moduleName]);
+      }
+    }
+  }
 
   function generateModule(name, values) {
     define(name, [], function() {
@@ -256,6 +297,7 @@
 
   processEmberShims();
   processTestShims();
+  processEmberDataShims();
   generateModule('jquery', { 'default': self.jQuery });
   generateModule('rsvp', { 'default': Ember.RSVP });
 })();
